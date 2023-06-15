bc78ef607f04a6e39117ed48284f5460a18136b3
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-06-15 22:30:14 +0100

    Added moon icon

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,10 +1,14 @@
 import styles from './TitleBar.module.css';
+import moon from '../assets/icon-moon.svg';
 
 const TitleBar = () => {
   return (
     <div className={styles.title}>
       <div>devFinder</div>
-      <div>Dark</div>
+      <div className={styles.mode}>
+        <div>Dark</div>
+        <img src={moon} alt='' />
+      </div>
     </div>
   );
 };
