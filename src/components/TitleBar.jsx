7899f808f1fc443c57711877c1726b068c2b81e7
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-07-02 23:08:16 +0100

    Styling of title bar

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -4,11 +4,13 @@ import moon from '../assets/icon-moon.svg';
 const TitleBar = () => {
   return (
     <div className={styles.title}>
-      <div>devFinder</div>
-      <div className={styles.mode}>
+      <div>
+        <p className={styles.titletext}>devFinder</p>
+      </div>
+      {/* <div className={styles.mode}>
         <div>Dark</div>
         <img src={moon} alt='' />
-      </div>
+      </div> */}
     </div>
   );
 };
