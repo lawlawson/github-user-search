8d30ab28cac377a20f988423d44454fc258db99a
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-07-07 21:42:58 +0100

    Basic outline of stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,19 @@
+import styles from './Stats.module.css';
+
 const Stats = () => {
   return (
-    <div>
+    <div className={styles['container']}>
       <div>
         <p>Repos</p>
-        <p>8</p>
+        <h2>8</h2>
+      </div>
+      <div>
+        <p>Followers</p>
+        <h2>3938</h2>
+      </div>
+      <div>
+        <p>Following</p>
+        <h2>9</h2>
       </div>
     </div>
   );
