2f6caf91d0aec472a29cfa501739cb2f28e81511
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-07-11 23:19:58 +0100

    h1 and h2 fonts

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,15 +5,15 @@ const Stats = () => {
     <div className={styles['container']}>
       <div className={styles['repos']}>
         <p>Repos</p>
-        <h2>8</h2>
+        <h2 className={styles['stat-number']}>8</h2>
       </div>
       <div className={styles['followers']}>
         <p>Followers</p>
-        <h2>3938</h2>
+        <h2 className={styles['stat-number']}>3938</h2>
       </div>
       <div className={styles['following']}>
         <p>Following</p>
-        <h2>9</h2>
+        <h2 className={styles['stat-number']}>9</h2>
       </div>
     </div>
   );
